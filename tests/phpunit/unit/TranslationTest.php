4a2c8f35befba3441e23a4d71b802f71596a9e7d
lalbert <lucalbert66@gmail.com>
2015-12-09 15:15:34 +0100

    Optimize and modify tests

--- tests/phpunit/unit/TranslationTest.php
+++ tests/phpunit/unit/TranslationTest.php
@@ -19,6 +19,8 @@ class TranslationTest extends \PHPUnit_Framework_TestCase
         // Teste la case de la 1ère lettre
         $this->_testFirstLettreCase($en, $fr);
 
+        // Test la ponctuation
+        $this->_testFinalPunctuation($en, $fr);
     }
 
     /**
@@ -43,14 +45,20 @@ class TranslationTest extends \PHPUnit_Framework_TestCase
     {
         // Before
         if(\strlen($en) !== \strlen(\ltrim($en))) {
-            $this->assertNotEquals($fr, \ltrim($fr), "Un espace en début de chaîne n'est pas respecté dans la traduction.");
+            // Ignore si le premier caractère est "\n"
+            if(10 != \ord($en)) {
+                $this->assertNotEquals($fr, \ltrim($fr), "Un espace en début de chaîne n'est pas respecté dans la traduction.");
+            }
         } else {
             $this->assertEquals($fr, \ltrim($fr), "Un espace en début de chaîne semble avoir été ajouté dans la traduction.");
         }
 
         // After
         if(\strlen($en) !== \strlen(\rtrim($en))) {
-            $this->assertNotEquals($fr, \rtrim($fr), "Un espace en fin de chaîne n'est pas respecté dans la traduction.");
+            // Ignore si le premier caractère est "\n"
+            if(10 != \ord($en)) {
+                $this->assertNotEquals($fr, \rtrim($fr), "Un espace en fin de chaîne n'est pas respecté dans la traduction.");
+            }
         }
     }
 
@@ -65,18 +73,45 @@ class TranslationTest extends \PHPUnit_Framework_TestCase
      */
     protected function _testFirstLettreCase($en, $fr)
     {
-        // Supprime les espaces
+        $ignoreBeginBy = ['eBay', 'entity_type'];
+
+        // Supprime les espaces du début
         $en = \ltrim($en);
         $fr = \ltrim($fr);
 
-        $firstLetterEn = \substr($en, 0, 1);
-        $firstLetterFr = \substr($fr, 0, 1);
+        $firstWordEn = current(explode(' ', $en));
+        $firstWordFr = current(explode(' ', $fr));
 
-        if(\ctype_alpha($firstLetterEn) && \ctype_alpha($firstLetterFr)) {
-            if(\ctype_upper($firstLetterEn)) {
-                $this->assertTrue(\ctype_upper($firstLetterFr), 'La 1ère lettre de la traduction devrait être en majuscule.');
-            } elseif(\ctype_lower($firstLetterEn)) {
-                $this->assertTrue(\ctype_lower($firstLetterFr), 'La 1ère lettre de la traduction devrait être en minuscule.');
+        $isIgnored = \in_array($firstWordEn, $ignoreBeginBy) || \in_array($firstWordFr, $ignoreBeginBy);
+
+        if(!$isIgnored) {
+            $firstLetterEn = \substr($en, 0, 1);
+            $firstLetterFr = \substr($fr, 0, 1);
+
+            if(\ctype_alpha($firstLetterEn) && \ctype_alpha($firstLetterFr)) {
+                if(\ctype_upper($firstLetterEn)) {
+                    $this->assertTrue(\ctype_upper($firstLetterFr), 'La 1ère lettre de la traduction devrait être en majuscule.');
+                } elseif(\ctype_lower($firstLetterEn)) {
+                    $this->assertTrue(\ctype_lower($firstLetterFr), 'La 1ère lettre de la traduction devrait être en minuscule.');
+                }
+            }
+        }
+    }
+
+    public function _testFinalPunctuation($en, $fr)
+    {
+        $ignoredPunctuation = ['#', '°', ')', '"', '>'];
+
+        // Supprime les espaces de fin
+        $en = \rtrim($en);
+        $fr = \rtrim($fr);
+
+        $lastLetterEn = \substr($en, -1);
+        $lastLetterFr = \substr($fr, -1);
+
+        if(!\in_array($lastLetterEn, $ignoredPunctuation) && \ctype_punct($lastLetterEn)) {
+            if(!\in_array($lastLetterFr, $ignoredPunctuation)) {
+                $this->assertTrue(\ctype_punct($lastLetterFr), "Il semble qu'il manque la ponctuation de la fin de chaîne.");
             }
         }
     }
